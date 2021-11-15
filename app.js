462d2ae5c6bc98e3c87d23ab84cb2a6f138d772e
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-11-15 21:07:49 +0100

    test: add get and post users automated tests

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// Log requests (except in test mode).
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
