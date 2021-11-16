e6cf2befd93f16b896811925469a05fd4faeefcc
Steve Mettraux <steve.mettraux@heig-vd.ch>
2021-11-16 09:25:28 +0100

    fix: update config.js for heroku deployment

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ try {
   }
 
 // Retrieve configuration from environment variables.
-exports.port = process.env.PORT || 3000;
+exports.port = process.env.PORT || 27017;
 exports.secretKey = process.env.SECRET_KEY || 'changeme';
 exports.url_prefix = process.env.URL_PREFIX;
-exports.database_server = process.env.DATABASE_SERVER;
+exports.database_server = process.env.DATABASE_SERVER || process.env.DATABASE_URL;
 exports.database_name = process.env.DATABASE_NAME;
